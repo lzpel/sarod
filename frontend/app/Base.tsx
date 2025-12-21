70f74a1900bc0196121435eccb341338c81fb3e3
Satoshi Misumi <18492524+lzpel@users.noreply.github.com>
2025-12-22 04:35:06 +0900

    feature/update (#11)

--- frontend/app/Base.tsx
+++ frontend/app/Base.tsx
@@ -1,6 +1,7 @@
 //- TabsWithDataKey.tsxを用いてホーム、ランキング、スター、ユーザーの四つを横に並べて、またコンテンツはタブの下に接しています
 //- nuqsを用いて現在のタブを管理します
 //- タブの下にheight:100%の領域がありそこにchildrenが入ります
+//- TabsWithDataKeyのclassNameを用いてタブを中央に寄せて、またタブ(frontend\stateless_ui\IconWithLabel.tsx)を大きめにし、間隔を広げます
 //全体ルール：
 //- UI部品はexport function/export default functionで構築、constに関数を入れるのは禁止
 //- ... function ... (props: ...){ props.要素 }のように引数を宣言する。... function ... ({...}:型)のように引数を宣言しない。
@@ -17,20 +18,22 @@ import { useQueryState, parseAsString } from 'nuqs';
 import TabsWithDataKey from '@/stateless_ui/TabsWithDataKey';
 import IconWithLabel from '@/stateless_ui/IconWithLabel';
 import { Home, Trophy, Star, User } from 'lucide-react';
+import { useAuth } from '@/src/AuthProvider';
+import Redirect from '@/stateless_ui/Redirect';
+import { User as IAM } from '@/src/out';
 
-type BaseProps = {
-	children: React.ReactNode;
-};
-
-export default function Base(props: BaseProps) {
-	return (
-		<Suspense fallback={<div>Loading...</div>}>
-			<BaseContent>{props.children}</BaseContent>
+export default function Base(props: {children: React.ReactNode}) {
+	const {iam, loading} = useAuth();
+	if (iam) {
+		return <Suspense fallback={<div>Loading...</div>}>
+			<BaseContent user={iam}>{props.children}</BaseContent>
 		</Suspense>
-	);
+	} else {
+		return <Redirect target="/signin" />
+	}
 }
 
-function BaseContent(props: BaseProps) {
+function BaseContent(props: {children: React.ReactNode, user: IAM}) {
 	const [page, setPage] = useQueryState('page', parseAsString.withDefault('home'));
 
 	const handleTabClick = (key: string) => {
@@ -40,12 +43,12 @@ function BaseContent(props: BaseProps) {
 	return (
 		<div className="flex flex-col h-screen w-full bg-background-default text-text-primary">
 			<div className="flex-none border-b border-divider">
-				<TabsWithDataKey contentSide="bottom">
+				<TabsWithDataKey contentSide="bottom" className="justify-center">
 					<div
 						key="home"
 						onClick={() => handleTabClick('home')}
 						aria-selected={page === 'home'}
-						className="flex-1 flex justify-center w-full"
+						className="flex justify-center"
 					>
 						<IconWithLabel icon={<Home />} label="ホーム" />
 					</div>
@@ -53,7 +56,7 @@ function BaseContent(props: BaseProps) {
 						key="ranking"
 						onClick={() => handleTabClick('ranking')}
 						aria-selected={page === 'ranking'}
-						className="flex-1 flex justify-center w-full"
+						className="flex justify-center"
 					>
 						<IconWithLabel icon={<Trophy />} label="ランキング" />
 					</div>
@@ -61,7 +64,7 @@ function BaseContent(props: BaseProps) {
 						key="star"
 						onClick={() => handleTabClick('star')}
 						aria-selected={page === 'star'}
-						className="flex-1 flex justify-center w-full"
+						className="flex justify-center"
 					>
 						<IconWithLabel icon={<Star />} label="スター" />
 					</div>
@@ -69,7 +72,7 @@ function BaseContent(props: BaseProps) {
 						key="user"
 						onClick={() => handleTabClick('user')}
 						aria-selected={page === 'user'}
-						className="flex-1 flex justify-center w-full"
+						className="flex justify-center"
 					>
 						<IconWithLabel icon={<User />} label="ユーザー" />
 					</div>
--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "@/globals.css";
 import AuthProvider from "@/src/AuthProvider";
+import { NuqsAdapter } from "nuqs/adapters/react";
 
 const geistSans = Geist({
 	variable: "--font-geist-sans",
@@ -31,7 +32,9 @@ export default function RootLayout({
 		<html lang="jp">
 			<body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
 				<AuthProvider>
-					{children}
+					<NuqsAdapter>
+						{children}
+					</NuqsAdapter>
 				</AuthProvider>
 			</body>
 		</html>
